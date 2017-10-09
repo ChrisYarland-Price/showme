701398af371fc36119f203fa361dea47878e3be6
Chris Yarland-Price <chris.yarland@protonmail.com>
2017-10-09 16:00:21 +0100

    Working login for all spartans.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :spartans
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # root 'spartan#index'
 end
