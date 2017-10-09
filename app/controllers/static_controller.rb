a775487d626272967cf33e96bb44d4c3c01696df
Martha Conway <marthaconway93@gmail.com>
2017-10-09 17:21:44 +0100

    added methods to static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,18 @@
 class StaticController < ApplicationController
 	def home
+		case
+		when current_spartan.admin
+			admin_home
+		else
+			user_home
+		end
+	end
+
+	def admin_home
+		render "spartans/index"
+	end
+
+	def user_home
+		render "projects/index"
 	end
 end
