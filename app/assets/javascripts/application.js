ecceb62d885e5eddfd912b59960f15eb5c7792b6
Liam North <liam_north@outlook.com>
2017-10-12 14:36:12 +0100

    Requirements added in order to logout on heroku

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
+//= require popper
 //= require_tree .
